16adc6b3baeee2fbd94ee681255ec151f5dcc391
Kevan <kevinjbritten@gmail.com>
2020-10-16 08:57:24 -0400

    removed --open from dev script package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  entry: "main.js",
+  entry: "./main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist/",
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.html$/,
-        loader: "raw-loader"
+        loader: "file-loader"
       }
       
     ]
