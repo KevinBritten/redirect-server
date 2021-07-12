25def1de34e6b7b3a158eec77be793c1bbb168d6
Fern knee <kevinjbritten@gmail.com>
2021-07-12 16:20:57 -0400

    added readme and close tab functionality.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Redirect Server
+
+Monitor a site on multiple devices using webpack dev server. Usefull for seeing the same site on different size screens or operating systems.
+
+# INSTALL
+
+The redirect server requires you to have node and npm installed
+
+1. `git clone https://github.com/KevinBritten/redirect-server.git`
+2. `npm i`
+
+#USEAGE
+
+1. `webpack run dev`
+2. `node siteloader.js *URL*`
+
+replace *URL* with the site you want to visit. If the url does not contain "http", then "https://" will be prepended.
+Open the specified url with any device (may not work on all devices).
+
+To update the site being viewed, run `node siteloader.js *URL*` with a new url.
+
+
+
--- siteLoader.js
+++ siteLoader.js
@@ -1,20 +1,47 @@
+'use strict';
+
+const port = '3000'
+var localIP = Object.values(require("os").networkInterfaces())
+.flat()
+.filter(({ family, internal }) => family === "IPv4" && !internal)
+.map(({ address }) => address)
+
+
+const { networkInterfaces } = require('os');
 const fs = require('fs')
+
+const nets = networkInterfaces();
+// const results = Object.create(null); // Or just '{}', an empty object
+
+// function setIP() {
+
+//   for (const name of Object.keys(nets)) {
+//       for (const net of nets[name]) {
+//           // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+//           if (net.family === 'IPv4' && !net.internal) {
+//               if (!results[name]) {
+//                   results[name] = [];
+//               }
+//               results[name].push(net.address);
+//           }
+//       }
+//   }
+//   localIP = results["en0"][0]
+
+// }
+
+// setIP()
 
 let url = process.argv[2].includes('http') ? process.argv[2] : 'https://' + process.argv[2]
 
-// if (process.argv[2].includes('http')) {
-//     let url = process.argv[2]
-// } else {
-//     let url = 'https://' + process.argv[2]
-// }
 
 const data = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
     <script type="text/javascript">
-      const site = '${url}';
-      window.open(site);
+    window.close('openedSite')
+      window.open('${url}', 'openedSite');
     </script>
     <title>redirect</title>
   </head>
@@ -28,5 +55,8 @@ const data = `<!DOCTYPE html>
 fs.writeFile('index.html', data, (err) => {
     if (err) throw err;
     // console.log('Loading your site...');
-    console.log(url);
+    console.log('Loading ' + '\x1b[33m', url, "\x1b[0m");
+    console.log('Open the following address on any device you want to display the page: ' + '\x1b[32m',
+   `
+  ${localIP}:${port}`, "\x1b[0m")
   });
